2ba4230c8fd27fafc3a4263eb75e4254e423ad15
Molly Burns <Molly@Kathryns-MacBook-Pro.local>
2018-02-21 18:17:59 -0500

    magic number and images in code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,43 @@
-$ready
+
+
+
+
+$(document).ready(function () {
+    var wins = 0;
+    var losses = 0;
+    var minNumber = 20;
+    var maxNumber = 120;
+
+    var randomNumber = randomNumberFromRange(minNumber, maxNumber);
+
+
+    function randomNumberFromRange(min, max) {
+        return Math.floor(Math.random() * (max - min + 1) + 18);
+    }
+
+    console.log(randomNumber);
+    $("#Magic-Number").text(randomNumber);
+
+    //if $("#crystal1").click($("#crystal1")))
+    //var minCrystal = 1;
+    //var maxCrystal = 12;
+    //    var randCrystal1 = randCrystal1FromRange(minCrystal, maxCrystal);
+    //    function randomCrystal1FromRange(min, max)
+    //{
+    //        return Math.floor(Math.random() * (max - min + 1) + min);
+    //        console.log(randCrystal1);
+    //    }
+
+        var crystal1 = $("#crystal1"),
+        count = 0;
+        crystal1.click = function() {
+        count += 1;
+        console.log(count);
+        //crystal1.text = $("#your-number") + count;
+      };
+$("#wins").text(wins);
+$("#losses").text(losses);
+
+    
+    
+});
